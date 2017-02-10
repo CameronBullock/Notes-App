a661635e40b90721c3ef52149c547c643eeb97e8
emRae <emRae@users.noreply.github.com>
2017-02-09 21:02:29 -0700

    route and view changes for singular note

--- routes/notes.js
+++ routes/notes.js
@@ -18,8 +18,8 @@ router.post('/', function(req, res) {
 });
 
 router.get('/:id', function(req, res) {
-  Note.findById(req.params.id, function(err, note) {
-    res.render('notes', { note: note });
+  Notes.findById(req.params.id, function(err, note) {
+    res.render('note', { note: note });
   });
 });
 
